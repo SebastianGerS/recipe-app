b128fbf9ecb298e780a8fa81cedd57524d14d774
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-02-14 00:10:08 +0100

    create models

--- app/User.php
+++ app/User.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App;
-
+use Tymon\JWTAuth\Constracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
+
     use Notifiable;
 
     /**
@@ -26,4 +27,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
